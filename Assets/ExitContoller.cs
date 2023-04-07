50a10efdf9462d47c643ba1db73655d883afe243
tempname8900 <avpanferov92@gmail.com>
2023-04-07 23:52:05 +0300

    bonerloot

--- Assets/ExitContoller.cs
+++ Assets/ExitContoller.cs
@@ -6,6 +6,10 @@ public class ExitContoller : MonoBehaviour
 {
     public GameObject KeyPast;
     public GameObject KeyFuture;
+    public GameObject KeyPastButton;
+    public GameObject KeyFutureButton;
+    public Mesh Future;
+    public Mesh Past;
     public bool KeyPastInserted = false;
     public bool KeyFutureInserted = false;
     // Start is called before the first frame update
